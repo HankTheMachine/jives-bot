8b10c057f34b23bfe40ba4f42247e2ddcda5cc85
Henri Kähkönen <hmtkah@utu.fi>
2023-02-10 14:49:09 +0200

    (Semi-)automatisoitu push

---
+++ README.md
@@ -0,0 +1,8 @@
+# Jives-Bot
+
+## V 0.1.0.
+
+Jivesiin laitettu jotain perustoiminnallisuutta.
+
+Jivesin ohjaaminen toistaiseksi vain [Henkimaailma-Discordpalvelimella](https://discord.gg/GPtVSVaVz7)
+
--- auth.js
+++ auth.js
@@ -1,17 +1,14 @@
 //JIVES AUTH
 
-//Vaikka tämä on gitignoressa älä silti jätä tänne mitään ennen pushaamista
+//Yleisiä muuttujia tallennettu tänne
 
 export const auth = {
-
   //ei niin tulenarat
   levyRaatiChannel : "1070620790816510003",
   levyRaatiSubmissionsChannel : "1070620859246583808",
   HenKonenDiscordId : "235688525779959809",
   spotifyShareLink : "https://open.spotify.com/album/",
   spotifyApiAlbumLink : "https://api.spotify.com/v1/albums/",
-
-
 }
 
 
